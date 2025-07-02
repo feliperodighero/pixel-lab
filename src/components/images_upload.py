18e329b55aecc5ad274ca619ab76a7e0b22a5825
feliperodighero <feliperzarichta@gmail.com>
2025-07-02 19:47:21 -0300

    extends file format

--- src/components/images_upload.py
+++ src/components/images_upload.py
@@ -5,11 +5,11 @@ def upload_images():
     upload_col1, upload_col2 = st.columns(2)
     with upload_col1:
         first_uploaded_image = st.file_uploader(
-            "Escolha uma imagem", type=["jpg", "png", "jpeg"]
+            "Escolha uma imagem", type=["jpg", "png", "jpeg", "bmp", "tiff", "tif"]
         )
     with upload_col2:
         second_uploaded_image = st.file_uploader(
-            "Escolha outra imagem", type=["jpg", "png", "jpeg"]
+            "Escolha outra imagem", type=["jpg", "png", "jpeg", "bmp", "tiff", "tif"]
         )
 
     return first_uploaded_image, second_uploaded_image
